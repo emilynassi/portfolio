e5c2b703ebe23fb0a0b848f86966a5319c469c68
Emily Rosenkranz <emilyrosenkranz@Emilys-MacBook-Pro.local>
2020-07-26 16:09:45 -0400

    begin creating vue components for portfolio

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+   <header>
+    <h1>Emily Rosenkranz</h1>
+    <nav>
+      <a href="#about">About Me</a>
+      <a href="#work">Work</a>
+      <a href="#contact">Contact Me</a>
+      <a
+        href="https://docs.google.com/document/d/16-55lyIlM9nRUx1U9H9ajUxr4-ykV1B4FzWz6YvrwLc/edit?usp=sharing"
+        >Resume</a
+      >
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: {
+    headline: String
+  }
+};
+</script>
